d524e949b64ddfbf6462dc372971f4bb52408571
austinerb <austin.erb8@gmail.com>
2022-03-21 21:02:24 -0400

    minor style fixes

--- scripts.js
+++ scripts.js
@@ -17,11 +17,11 @@ let navMobileMenuBtn = document.querySelector(".nav-menu-btn");
 let navMobileMenuBars = document.querySelector(".bars");
 let navMobileMenuClose = document.querySelector(".close");
 let navMobileMenu = document.querySelector(".nav-menu");
-let content = document.querySelector(".content");
+// let content = document.querySelector(".content");
 
 navMobileMenuBtn.addEventListener("click", function() {
   navMobileMenuBars.classList.toggle("display-none");
   navMobileMenuClose.classList.toggle("display-none");
   navMobileMenu.classList.toggle("display-none");
-  content.classList.toggle("display-none");
+  // content.classList.toggle("display-none");
 });
